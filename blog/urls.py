1b01cde40c78de6cee08ff85b3205e8dd11451bc
Timothy O'Rourke <timothyjorourke1@gmail.com>
2021-06-14 23:54:47 -0700

    Post create view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('p/<str:pk>', views.PostDetailView.as_view(), name='post-detail'),
+    path('p/create', views.PostCreateView.as_view(), name='post-create'),
+    path('p/<str:pk>', login_required(views.PostDetailView.as_view()), name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
+from django.forms.models import model_to_dict
 
 # Create your views here.
 
+@login_required
 def index(request):
     sort = request.GET.get('sort', 'latest')
 
@@ -17,3 +22,13 @@ def index(request):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ('text',)
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.save()
+        return JsonResponse({ 'post' : model_to_dict(post) })
